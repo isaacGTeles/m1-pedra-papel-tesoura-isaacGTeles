396030f60fc92355b38fddfaca6d62fb96c4b798
isaacGTeles <isaac.gabriel1999@gmail.com>
2024-06-21 08:15:29 -0300

    atividade concluida

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+    if (player1 === player2) {
+        return "Empate!";
+    }
+    if (
+        (player1 === 'Pedra' && player2 === 'Tesoura') ||
+        (player1 === 'Papel' && player2 === 'Pedra') ||
+        (player1 === 'Tesoura' && player2 === 'Papel')
+    ) {
+        return "Jogador 1 venceu!";
+    }
+    return "Jogador 2 venceu!";
+}
+
+let result = playRockPaperScissor("Papel", "Papel")
